fd76f74036e6a1be41dcb94e662ad2779bd34231
Abigail Watson <abigailwatson@Abigails-Mac-mini-377974.local>
2018-07-09 01:21:09 -0500

    default to local collection (HIPAA compliant; but annoying to use)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-person',
-  version: '1.1.6',
+  version: '1.1.7',
   summary: 'HL7 FHIR Resource - Person',
   git: 'https://github.com/clinical-meteor/hl7-resource-person',
   documentation: 'README.md'
