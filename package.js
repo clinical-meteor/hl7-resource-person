a549fc26efef2c50214a2e70a2cb01d98efd5ac0
Abigail Watson <abigailwatson@Abigails-Mac-mini-377974.local>
2018-07-09 01:01:27 -0500

    default to local collection (HIPAA compliant; but annoying to use)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-person',
-  version: '1.1.4',
+  version: '1.1.5',
   summary: 'HL7 FHIR Resource - Person',
   git: 'https://github.com/clinical-meteor/hl7-resource-person',
   documentation: 'README.md'
@@ -12,6 +12,7 @@ Package.onUse(function (api) {
   api.use('meteor-platform');
   api.use('mongo');
   api.use('aldeed:simple-schema@1.3.3');
+  api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
   api.use('momentjs:moment@2.17.1');
   api.use('ecmascript@0.9.0');
@@ -19,16 +20,14 @@ Package.onUse(function (api) {
   api.use('http');
   api.use('react-meteor-data@0.2.15');
 
-  api.use('clinical:glass-ui@2.2.1');
+  api.use('clinical:glass-ui@2.2.4');
   api.use('clinical:extended-api@2.2.2');
   api.use('clinical:base-model@1.3.5');
   api.use('clinical:user-model@1.5.0');
-  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.1');
   api.use('clinical:hl7-resource-bundle@1.3.10');
   api.use('matb33:collection-hooks@0.7.15');
   api.use('clinical:alert@2.1.3');
-  
-  api.imply('clinical:user-model');  
 
   api.addFiles('lib/Persons.js', ['client', 'server']);
 
@@ -36,6 +35,10 @@ Package.onUse(function (api) {
   api.addFiles('server/initialize.js', 'server');
   api.addFiles('server/methods.js', 'server');
 
+  if(Package['clinical:fhir-vault-server']){
+    api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  }
+  
   api.export('Person');
   api.export('Persons');
   api.export('PersonSchema');
